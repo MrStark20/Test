375549c5fd1662702f727c0589b4727bb487024b
MrStark <swagnik.dhar11@gmail.com>
2023-04-29 19:04:45 +0530

    first commit of matrix problem

---
+++ Sample test/a.cpp
@@ -0,0 +1,9 @@
+// Git first time installation
+// git init
+// git remote add origin https://github.com/MrStark20/Test.git"
+
+// git feature 
+//git add "file name" or( "." for everything)
+//git commit -m "first commit"
+//git branch -M main (for first time only implementation)
+//git push -u origin main
